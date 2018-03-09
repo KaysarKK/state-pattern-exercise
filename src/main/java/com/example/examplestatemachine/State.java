6d040cc6b165e1451af6c262c62a034c30d1f914
KaysarKK <kassenovkaysar@gmail.com>
2018-03-09 21:38:16 +0600

    Revert "Exercise 1"
    This reverts commit 24e95412537871fc85beaf6a91264d57bff087d2.

--- src/main/java/com/example/examplestatemachine/State.java
+++ src/main/java/com/example/examplestatemachine/State.java
@@ -4,13 +4,9 @@ public abstract class State {
     protected StateContext sc;
     protected boolean accept = false;
     
-    public void actionA() {
-    	
-    }
+    public void actionA() {}
     
-    public void actionB() {
-    	
-    }
+    public void actionB() {}
     
     public boolean isAccept() {
         return this.accept;
